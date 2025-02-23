c66c8cf4a61cf55f5531c02b2f301ab023e198a9
koushik <koushik.m@unifo.in>
2025-02-23 18:38:53 +0530

    added skeleton

--- webapp/src/store/app-store.ts
+++ webapp/src/store/app-store.ts
@@ -4,43 +4,39 @@ import merge from "lodash/merge";
 import cloneDeep from "lodash/cloneDeep";
 
 const testConfig: TestConfig = {
-  servers: [
-    { name: "server-1", port: 8080 },
-    { name: "server-2", port: 8081 },
-  ],
+  servers: [],
   repo: {
-    name: "Dummy",
-    path: "/home/praveen/praveenUnifo/even/evenWsTestDemo/",
-    description: "Testing dummy websocket server",
-    startCommand: "PORT=$$$$ npm run start",
-    isSocketIo: false,
+    name: "",
+    path: "",
+    description: "",
+    startCommand: "",
+    isSocketIo: true,
   },
   message: {
     recommendationMessages: {
       student_join: {
-        action: "student_join",
-        id: "SSN111",
-        year: 3,
-        section: "A",
-        department: "CS",
+        action: "",
+        id: "",
+        year: 0,
+        section: "",
+        department: "",
       },
-      professor_join: { action: "professor_join", id: "SSNProf111" },
+      professor_join: { action: "", id: "" },
       professor_broadcast: {
-        action: "professor_broadcast",
-        stuDepartment: "CS",
-        stuYear: 3,
-        stuSection: "A",
-        message: "hii this is prof SSNProf111",
+        action: "",
+        stuDepartment: "",
+        stuYear: 0,
+        stuSection: "",
+        message: "",
       },
       student_send: {
-        action: "student_send",
-        profId: "SSNProf111",
-        message: "Hello Prof from SSN222",
+        action: "",
+        profId: "",
+        message: "",
       },
     },
     outboundMessageProcessor: {
       code: `const outboundMessageProcessor = (message) => {
-  //Converts binary to json
   const decoder = new TextDecoder();
   const jsonString = decoder.decode(message);
   message = JSON.parse(jsonString);
@@ -50,7 +46,6 @@ const testConfig: TestConfig = {
     },
     inboundMessageProcessor: {
       code: `const inboundMessageProcessor = (message) => {
-  //Converts json to binary
   const jsonString = JSON.stringify(message);
   const encoder = new TextEncoder();
   message = encoder.encode(jsonString);
@@ -61,116 +56,32 @@ const testConfig: TestConfig = {
   },
   testSuites: [
     {
-      name: "Test suite 1",
-      description: "When a student is added/leaves other students should receive message.",
-      id: "100",
+      name: "",
+      description: "",
+      id: "",
       testCases: [
         {
-          action: "addUser",
-          testDescription: "Adding SSN111 to server-1",
-          testId: "100-1",
-          userName: "SSN111",
-          server: "server-1",
+          action: "",
+          testDescription: "",
+          testId: "",
+          userName: "",
+          server: "",
           consumedBy: [],
-          addUserMessage: { "action": "student_join", "id": "SSN111", "year": 3, "section": "A", "department": "CS" },
-          timeout: 5
-        },
-        {
-          action: "addUser",
-          testId: "100-2",
-          testDescription: "Adding SSN222 to server-1",
-          userName: "SSN222",
-          server: "server-1",
-          consumedBy: [{ users: ["SSN111"], message: { "action": "student_join", "id": "SSN222", "year": 3, "section": "A", "department": "CS" } }],
-          addUserMessage: { "action": "student_join", "id": "SSN222", "year": 3, "section": "A", "department": "CS" },
-          timeout: 5
-        },
-        {
-          action: "sendMessage",
-          testId: "100-3",
-          testDescription: "Sending message from SSNProf111 to SSN111 and SSN222",
-          timeout: 5,
-          producedBy: {
-            name: "SSNProf111",
-            message: { "action": "professor_broadcast", "stuDepartment": "CS", "stuYear": 3, "stuSection": "A", "message": "hii this is prof SSNProf111" },
-          },
-          consumedBy: [
-            {
-              users: ["SSN111", "SSN222"],
-              message: { "action": "professor_broadcast", "stuDepartment": "CS", "stuYear": 3, "stuSection": "A", "message": "hii this is prof SSNProf111" },
-            },
-          ],
-        },
-        {
-          action: "removeUser",
-          testDescription: "Removing SSN111. This should be notified to SSN222",
-          testId: "100-4",
-          name: "SSN111",
-          timeout: 5,
-          consumedBy: [
-            {
-              users: ["SSN222"],
-              message: { "action": "student_leave", "id": "SSN111", "year": 3, "section": "A", "department": "CS" },
-            },
-          ],
+          addUserMessage: {},
+          timeout: 0,
         },
       ],
     },
-    {
-      name: "Test suite 2",
-      description: "When a student is added/leaves other students should receive message.",
-      id: "100",
-      testCases: [
-        {
-          action: "addUser",
-          testDescription: "Adding SSN111 to server-1",
-          testId: "100-1",
-          userName: "SSN111",
-          server: "server-1",
-          consumedBy: [],
-          addUserMessage: { "action": "student_join", "id": "SSN111", "year": 3, "section": "A", "department": "CS" },
-          timeout: 5
-        },
-        {
-          action: "sendMessage",
-          testId: "100-3",
-          testDescription: "Sending message from SSNProf111 to SSN111 and SSN222",
-          timeout: 5,
-          producedBy: {
-            name: "SSNProf111",
-            message: { "action": "professor_broadcast", "stuDepartment": "CS", "stuYear": 3, "stuSection": "A", "message": "hii this is prof SSNProf111" },
-          },
-          consumedBy: [
-            {
-              users: ["SSN111", "SSN222"],
-              message: { "action": "professor_broadcast", "stuDepartment": "CS", "stuYear": 3, "stuSection": "A", "message": "hii this is prof SSNProf111" },
-            },
-          ],
-        },
-        {
-          action: "removeUser",
-          testDescription: "Removing SSN111. This should be notified to SSN222",
-          testId: "100-4",
-          name: "SSN111",
-          timeout: 5,
-          consumedBy: [
-            {
-              users: ["SSN222"],
-              message: { "action": "student_leave", "id": "SSN111", "year": 3, "section": "A", "department": "CS" },
-            },
-          ],
-        },
-      ],
-    }
   ],
-  totalTestCases: 18,
+  totalTestCases: 0,
   dataScript: {
-    init: `sudo snap start redis`,
-    cleanUp: `sudo snap stop redis`,
-    initTimeout: 5,
-    cleanUpTimeout: 5
-  }
+    init: "",
+    cleanUp: "",
+    initTimeout: 0,
+    cleanUpTimeout: 0,
+  },
 };
+
 
 interface TestConfigState {
   testConfig: TestConfig;
